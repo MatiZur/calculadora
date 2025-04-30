952659512d5f62753471541f80a2c843f42abd3a
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-04-30 09:43:59 -0300

    agregada validacion de datos en la raiz

--- Calculadora.c
+++ Calculadora.c
@@ -21,14 +21,34 @@ void divi(float *num1, float *num2){
 	printf("\nLa division entre %f y %f da %f.\n\n\n", *num1, *num2, res);
 }
 
-void potencia(float *num1, float *num2){
-	float res = pow(*num1, *num2);
-	printf("\nLa potencia de %f de exponente %f da %f.\n\n\n", *num1, *num2, res);
+void potencia(){
+	float num1, num2;
+	printf("\nIngrese un numero: ");
+	scanf("%f",&num1);
+	printf("Ingrese el exponente: ");
+	scanf("%f",&num2);
+	float res = pow(num1, num2);
+	printf("\nLa potencia de %f de exponente %f da %f.\n\n\n", num1, num2, res);
 }
 
-void raiz(float *num1, float *num2){
-	float res = pow(*num1, (1 / *num2));
-	printf("\nLa raiz de %f de indice %f da %f.\n\n\n", *num1, *num2, res);
+void raiz(){
+	float num1, num2;
+	do{
+		printf("\nIngrese un numero: ");
+		scanf("%f",&num1);
+		if(num1<0){
+			printf("\nEl numero no puede ser negativo.\n");
+		}
+	}while(num1<0);
+	do{
+		printf("Ingrese el indice: ");
+		scanf("%f",&num2);
+		if(num2==0){
+			printf("\nEl indice no puede ser 0.\n\n");
+		}
+	}while(num2==0);
+	float res = pow(num1, (1 / num2));
+	printf("\nLa raiz de %f de indice %f da %f.\n\n\n", num1, num2, res);
 }
 
 int main(){
@@ -41,17 +61,6 @@ int main(){
 			printf("\nIngrese un numero: ");
 			scanf("%f",&num1);
 			printf("Ingrese otro numero: ");
-			scanf("%f",&num2);
-		}
-		else if(opcion>=5 && opcion<=6){
-			printf("\nIngrese un numero: ");
-			scanf("%f",&num1);
-			if(opcion==5){
-				printf("Ingrese el exponente: ");
-			}
-			else if(opcion==6){
-				printf("Ingrese el indice: ");
-			}
 			scanf("%f",&num2);
 		}
 		switch(opcion){
@@ -68,10 +77,10 @@ int main(){
 				divi(&num1, &num2);
 				break;
 			case 5:
-				potencia(&num1, &num2);
+				potencia();
 				break;
 			case 6:
-				raiz(&num1, &num2);
+				raiz();
 				break;
 			case 7:
 				printf("\nTerminando programa...");
