64ee7698838dfe4375314142e1c107d7372ea0f6
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-05-08 10:05:44 -0300

    sistema 2 por 2 hecho

--- Calculadora_sistemas.c
+++ Calculadora_sistemas.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 void sistema2(){
-	int multiUno1, multiDos1, res1, multiUno2, multiDos2, res2;
+	int multiUno1, multiDos1, res1, multiUno2, multiDos2, res2, multiInver;
 	float resUno, resDos, resTot, y, x;
 	printf("\nIngrese el multiplicador de la primer variable de la primer ecuacion: ");
 	scanf("%d",&multiUno1);
@@ -17,18 +17,10 @@ void sistema2(){
 	printf("Ingrese el resultado de la segunda ecuacion: ");
 	scanf("%d",&res2);
 	printf("\nLas ecuaciones son:\n%dx + %dy = %d\n%dx + %dy = %d", multiUno1, multiDos1, res1, multiUno2, multiDos2, res2);
-	if((multiUno1 > 0 && multiUno2 > 0) || (multiUno1 < 0 && multiUno2 < 0)){
-		int multiInver;
-		multiInver = -1 * multiUno1;
-		resUno = (multiUno2 * multiUno1) + (multiInver * multiUno2);
-		resDos = (multiUno2 * multiDos1) + (multiInver * multiDos2);
-		resTot = (multiUno2 * res1) + (multiInver * res2);
-	}
-	else{
-		resUno = (multiUno2 * multiUno1) + (multiUno1 * multiUno2);
-		resDos = (multiUno2 * multiDos1) + (multiUno1 * multiDos2);
-		resTot = (multiUno2 * res1) + (multiUno1 * res2);
-	}
+	multiInver = -1 * multiUno1;
+	resUno = (multiUno2 * multiUno1) + (multiInver * multiUno2);
+	resDos = (multiUno2 * multiDos1) + (multiInver * multiDos2);
+	resTot = (multiUno2 * res1) + (multiInver * res2);
 	y = resTot/resDos;
 	x = (res1 - (multiDos1 * y)) / multiUno1;
 	printf("\n\ny es igual a %f.\nx es igual a %f.\n\n\n",y,x);
@@ -52,6 +44,7 @@ int main(){
 				break;
 			case 3:
 				printf("\nTerminando programa...");
+				break;
 			default:
 				printf("\nOpcion no reconocida\n\n\n");
 				break;
