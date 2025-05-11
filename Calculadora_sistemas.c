6e2907cceeb4c0f0776216104bce52568be4aec8
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-05-11 16:14:09 -0300

    sistemas terminado
    agregado 3x3, cambiados nombre de variables y pido de las mismas

--- Calculadora_sistemas.c
+++ Calculadora_sistemas.c
@@ -9,8 +9,8 @@ void sistema2(){
 	printf("\nIngrese los coeficientes de la segunda ecuacion (a2 b2 r2): ");
 	scanf("%d %d %d", &a2, &b2, &r2);
 	printf("\nLas ecuaciones son:\n%dx + %dy = %d\n%dx + %dy = %d", a1, b1, r1, a2, b2, r2);
-    resB = (a2 * b1) - (a1 * b2);
-    resR = (a2 * r1) - (a1 * r2);
+	resB = (a2 * b1) - (a1 * b2);
+	resR = (a2 * r1) - (a1 * r2);
 	y = resR / resB;
 	x = (r1 - (b1 * y)) / a1;
 	printf("\n\nx es igual a %f.\ny es igual a %f.\n\n\n",x,y);
@@ -27,13 +27,13 @@ void sistema3(){
 	scanf("%d %d %d %d", &a3, &b3, &c3, &r3);
 	printf("\nLas ecuaciones son:\n%dx + %dy + %dz = %d\n%dx + %dy + %dz = %d\n%dx + %dy + %dz = %d", a1, b1, c1, r1, a2, b2, c2, r2, a3, b3, c3, r3);
 	resB1 = (a2 * b1) - (a1 * b2);
-    resC1 = (a2 * c1) - (a1 * c2);
-    resR1 = (a2 * r1) - (a1 * r2);
+	resC1 = (a2 * c1) - (a1 * c2);
+	resR1 = (a2 * r1) - (a1 * r2);
 	resB2 = (a3 * b1) - (a1 * b3);
-    resC2 = (a3 * c1) - (a1 * c3);
-    resR2 = (a3 * r1) - (a1 * r3);
-    finalC = (resB2 * resC1) - (resB1 * resC2);
-    finalR = (resB2 * resR1) - (resB1 * resR2);
+	resC2 = (a3 * c1) - (a1 * c3);
+	resR2 = (a3 * r1) - (a1 * r3);
+	finalC = (resB2 * resC1) - (resB1 * resC2);
+	finalR = (resB2 * resR1) - (resB1 * resR2);
 	if(finalC == 0 || resB1 == 0 || a1 == 0){
     	printf("\n\nEl sistema no tiene solucion unica o hay una division por cero.\n\n\n");
     	return;
