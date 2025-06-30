7768e1d964d9cc3d7117a31f58fc84d5332e98bb
MatiZur <166444039+MatiZur@users.noreply.github.com>
2025-06-29 23:47:39 -0300

    añadidos nuestros nombres !!

--- LaCalcularda/src/gui/VentanaPrincipal.java
+++ LaCalcularda/src/gui/VentanaPrincipal.java
@@ -21,7 +21,7 @@ public class VentanaPrincipal extends JFrame {
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("La Calcularda");
-		setBounds(100, 100, 450, 300);
+		setSize(600, 300);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -31,34 +31,48 @@ public class VentanaPrincipal extends JFrame {
 		JLabel lblCalcularda = new JLabel("La Calcularda");
 		lblCalcularda.setForeground(new Color(0, 0, 0));
 		lblCalcularda.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCalcularda.setBounds(0, 31, 434, 39);
+		lblCalcularda.setBounds(0, 28, 584, 39);
 		lblCalcularda.setFont(new Font("Tahoma", Font.BOLD, 32));
 		contentPane.add(lblCalcularda);
 		
 		JButton btnNumeros = new JButton("Números");
-		btnNumeros.setBounds(83, 107, 100, 30);
+		btnNumeros.setBounds(58, 140, 100, 30);
 		contentPane.add(btnNumeros);
 		btnNumeros.addActionListener(e -> {
 			new VentanaNumeros().setVisible(true);
 		});
 		
 		JButton btnVectores = new JButton("Vectores");
-		btnVectores.setBounds(243, 107, 100, 30);
+		btnVectores.setBounds(181, 140, 100, 30);
 		contentPane.add(btnVectores);
 		btnVectores.addActionListener(e -> {
 			new VentanaVectores().setVisible(true);
 		});
 		
 		JButton btnMatrices = new JButton("Matrices");
-		btnMatrices.setBounds(83, 158, 100, 30);
+		btnMatrices.setBounds(309, 140, 100, 30);
 		contentPane.add(btnMatrices);
 		btnMatrices.addActionListener(e -> {
 			new VentanaMatrices().setVisible(true);
 		});
 		
 		JButton btnSistemas = new JButton("Sistemas");
-		btnSistemas.setBounds(243, 158, 100, 30);
+		btnSistemas.setBounds(433, 140, 100, 30);
 		contentPane.add(btnSistemas);
+		
+		JLabel lblNewLabel = new JLabel("Desarrollado por Santiago Papale, Octavio Romero, Leandro Zelaya y Matias Zurita");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBackground(new Color(0, 0, 0));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(0, 222, 584, 28);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblconQuDesea = new JLabel("Aprete el botón correspondiente a lo que desea trabajar:");
+		lblconQuDesea.setHorizontalAlignment(SwingConstants.CENTER);
+		lblconQuDesea.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblconQuDesea.setBackground(Color.BLACK);
+		lblconQuDesea.setBounds(0, 101, 584, 28);
+		contentPane.add(lblconQuDesea);
 		btnSistemas.addActionListener(e -> {
 			new VentanaSistemas().setVisible(true);
 		});
